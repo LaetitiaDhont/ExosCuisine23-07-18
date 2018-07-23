75090929428007c6bc36e4922fdfc8582ed9d000
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-07-23 17:33:39 +0200

    first commit upload all files

---
+++ ExCuisine230718/src/package1/Application.java
@@ -0,0 +1,36 @@
+package package1;
+// exemple de création d'instance
+
+// j'ai ma classe Cuisine pour me préparer un café
+
+// je créer une deuxième classe 
+class Application {
+	
+	public static void main(String[] args) {
+		// création d'une instance de la classe cuisine nommée "cuisineSimplon" 
+		// exemple avec la classe Scanner, c'est exactement la même chose
+		Cuisine cuisineSimplon = new Cuisine();
+		// on veut avoir accès à nos variables de la class Cuisine, en l'occurence la tasse
+		// le point permet de faire le lien
+		cuisineSimplon.tasse = "Mug";
+		cuisineSimplon.nom = "Serge";
+		cuisineSimplon.prenom ="Janitor";
+		
+		Cuisine cuisinePierre = new Cuisine();
+		cuisinePierre.tasse = "Peter Pan";
+		cuisinePierre.nom = "Brogard";
+		cuisinePierre.prenom = "Pierre";
+		
+		Cuisine cuisineAthanasia = new Cuisine();
+		cuisineAthanasia.tasse = "Fée Clochette";
+		cuisineAthanasia.nom = "Katsouraki";
+		cuisineAthanasia.prenom ="Athanasia";
+		
+		String retourCafe;
+		//On appelle la méthode machineCafe de l'instance cuisineSimplon de la classe Cuisine et on stock son résultat
+		retourCafe = cuisineSimplon.machineCafe("Georges Clooney", "EauMG", "Benjamin");
+		retourCafe = cuisinePierre.machineCafe("Colombien", "Cristaline", "On");
+		retourCafe = cuisineAthanasia.machineCafe("Arabica", "Volvic", "On");
+	
+	}
+}
---
+++ ExCuisine230718/src/package1/Cuisine.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class Cuisine {
+	
+		// j'ai mes variables tasse + nom et prénom du propriétaire de la cuisine
+		String tasse;
+		String nom;
+		String prenom;
+		
+		// je créer ma méthode pour faire mon café puis je retourne à ma variable café
+		String machineCafe (String cafe, String eau, String button) {
+			return cafe + eau + button; 
+		}
+	}
+
+
