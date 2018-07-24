3521be830f652383918251b98c528ac4dffdc1ca
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-07-24 11:41:14 +0200

    Nouvelle exo pour voir si on maîtrise les instances

---
+++ ExCuisine230718/src/package1/Cuisine1.java
@@ -0,0 +1,21 @@
+package package1;
+
+// J'ai créé ma classe qui va executer mon code avec la méthode main 
+
+public class Cuisine1 {
+
+	// J'ai créé 4 nouvelles instances 
+	Personne p1 = new Personne();
+	Personne p2 = new Personne();
+	
+	Verre v1 = new Verre();
+	Verre v2 = new Verre();
+	
+	// Ma méthode main
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
---
+++ ExCuisine230718/src/package1/Personne.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class Personne {
+
+	String nom;
+	String prenom;
+	String tel;
+	String email;
+
+}
